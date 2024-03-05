36bf3707d94228e868baabd5d1ade843fcd1c302
alienworld1 <astronomyguy0030@gmail.com>
2024-03-05 21:26:58 +0530

    Add reverseString and calculator

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
 
-export {capitalize};
+const reverseString = string => {
+    let reverse = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverse = reverse.concat(string.charAt(i));
+    }
+    return reverse;
+}
+
+const calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+}
+
+export {
+    capitalize,
+    reverseString,
+    calculator,
+};
--- app.test.js
+++ app.test.js
@@ -1,4 +1,8 @@
-import {capitalize} from './app';
+import {
+    capitalize,
+    reverseString,
+    calculator,
+} from './app';
 
 test('capitalize object to Object', () => (
     expect(capitalize('object')).toBe('Object')
@@ -14,4 +18,28 @@ test('string starting with a number remains unchanged', () => (
 
 test('string starting with a capital letter remains unchanged', () => (
     expect(capitalize('Object')).toBe('Object')
+));
+
+test('reverse object to tcejbo', () => (
+    expect(reverseString('object')).toBe('tcejbo')
+));
+
+test('reverse xyz to zyx', () => (
+    expect(reverseString('xyz')).toBe('zyx')
+));
+
+test('calculator.add(3, 4) gives 7', () => (
+    expect(calculator.add(3, 4)).toBe(7)
+));
+
+test('calculator.subtract(10, 3) gives 7', () => (
+    expect(calculator.subtract(10, 3)).toBe(7)
+));
+
+test('calculator multiply(3, 4) gives 12', () => (
+    expect(calculator.multiply(3, 4)).toBe(12)
+));
+
+test('calculator.divide(12, 4) gives 3', () => (
+    expect(calculator.divide(12, 4)).toBe(3)
 ));
